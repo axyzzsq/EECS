4e6ae7909e1ebbc164ef4422303b6a8a0311aa04
赵嗣庆 <Siqing_Zhao@intretech.com>
2022-11-03 15:27:51 +0800

    feat

--- 00_程序算法/C++程序设计/韦东山C++快速入门/SourceCode/05th_pointer_reference/main.cpp
+++ 00_程序算法/C++程序设计/韦东山C++快速入门/SourceCode/05th_pointer_reference/main.cpp
@@ -9,22 +9,23 @@ int add_one(int a)
 	return a;
 }
 
-int add_one(int *a)
+int add_one(int *a) //传址,可以对传进来的参数进行改值
 {
 	*a = *a + 1;
 	return *a;
 }
 
-int add_one_ref(int &b)
+int add_one_ref(int &b) //& 引用符
 {
 	b = b+1;
 	return b;
 }
 
+//引用是别名，初始化为某个变量的别名，二者指向同一块内存
 int main(int argc, char **argv)
 {
 	int a = 99;
-        int &c = a;
+    int &c = a; //引用一定要在定义的时候直接去初始化
 	cout<<add_one(a)<<endl;
 	cout<<"a = "<<a<<endl;
 
