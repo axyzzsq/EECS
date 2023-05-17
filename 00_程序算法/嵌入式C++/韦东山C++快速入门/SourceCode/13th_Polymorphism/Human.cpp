65e9bac18ce75a307e87b5039579bc19bf26b3b1
siqing <595216650@qq.com>
2023-05-17 20:05:57 +0800

    fix

--- 00_程序算法/嵌入式C++/韦东山C++快速入门/SourceCode/13th_Polymorphism/Human.cpp
+++ 00_程序算法/嵌入式C++/韦东山C++快速入门/SourceCode/13th_Polymorphism/Human.cpp
@@ -2,28 +2,41 @@
 #include <string.h>
 #include <unistd.h>
 
+/* 这个程序是为了说明多态的概念，多态的概念是指在继承关系中，
+   子类可以重写父类的方法，这样在调用的时候，就会根据对象的类型
+   来调用不同的方法*/
+
 using namespace std;
 
-class Human {
+class Human
+{
 public:
-	void eating(void) { cout<<"use hand to eat"<<endl; }
+	void eating(void) { cout << "use hand to eat" << endl; }
 };
 
-class Englishman : public Human {
+// Englishman类继承了Human类，重写了eating方法
+class Englishman : public Human
+{
 public:
-	void eating(void) { cout<<"use knife to eat"<<endl; }
+	void eating(void) { cout << "use knife to eat" << endl; }
 };
 
-
-class Chinese : public Human {
+// Chinese类继承了Human类，重写了eating方法
+class Chinese : public Human
+{
 public:
-	void eating(void) { cout<<"use chopsticks to eat"<<endl; }
+	void eating(void) 
+	{ 
+		cout << "use chopsticks to eat" << endl; 
+		}
 };
 
-void test_eating(Human& h)
+// test_eating函数的参数是Human类的引用，这样就可以传入Human类的
+void test_eating(Human &h)
 {
 	h.eating();
 }
+
 
 int main(int argc, char **argv)
 {
@@ -31,11 +44,9 @@ int main(int argc, char **argv)
 	Englishman e;
 	Chinese c;
 
-	test_eating(h);
-	test_eating(e);
-	test_eating(c);
+	test_eating(h); // 这里传入的是Human类的引用，但是实际上传入的是Englishman类的引用
+	test_eating(e); // 这里传入的是Human类的引用，但是实际上传入的是Chinese类的引用
+	test_eating(c); // 这里传入的是Human类的引用，但是实际上传入的Chinese类的引用
 
 	return 0;
 }
-
-
